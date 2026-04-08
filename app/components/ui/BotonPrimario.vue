a12b24f73e7f37cbdf4c7d4031d217bedc8be223
brenda <brendazaine@gmail.com>
2026-04-08 15:07:37 +0200

    correción de modulos desincronizados de node y cambio minimo en botón primario

--- app/components/ui/BotonPrimario.vue
+++ app/components/ui/BotonPrimario.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <button
     type="button"
-    class="group inline-flex rounded-full bg-white p-[2px]"
+    class="group inline-flex rounded-full bg-white p-px"
   >
     <span
       class="inline-flex min-w-[220px] items-center justify-center rounded-full bg-black px-8 py-3 text-sm font-semibold text-white group-hover:bg-gradient-to-r group-hover:from-pink-500 group-hover:via-purple-500 group-hover:to-cyan-400"
