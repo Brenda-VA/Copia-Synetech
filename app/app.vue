7d54460e817a021c04bf9c5809e136426bf280fc
brenda <brendazaine@gmail.com>
2026-04-10 11:59:50 +0200

    Navbar en app.vue para que se vea globalmente

--- app/app.vue
+++ app/app.vue
@@ -13,7 +13,9 @@
   Aqui se está moviendo el contenido a index, cuando no tenía más páginas app.vue contenia todo,
   ahora al tener mas páginas, Nuxt activa el sistema de rutas y espera páginas dentro de pages/
   en pages debe ir index.vue que ahora serria la home real, y dentro de pages cada nuevo archivo es una rutapara una pagins nueva--> 
-<template>
+<template> 
+<!-- A partir de ahora en app.vue deben ir cosas globales, por ejemplo dejar el navbar, esto haria q este elemento este en todas las páginas -->
+  <LayoutBarraNavegacion />
   <NuxtPage /> <!-- renderiza la página actual -->
-  <!-- A partir de ahora en app.vue deben ir cosas globales, por ejemplo dejar el navbar + footer, esto haria q esos dos elementos esten en todas las páginas -->
+ 
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <LayoutBarraNavegacion />
+    <!--Ya no esta la import del navbar pq se colocó eb app.vue como un elemento global, esto para tener la navbar en todas las páginas-->
   <SeccionesHeroCarrusel />
   <SeccionesSeccionCaracteristicasIA />
   <SeccionesSeccionSuiteEducativa />
