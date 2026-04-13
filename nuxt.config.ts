592ad719bf58aefa0504c88e300436da49c07486
brenda <brendazaine@gmail.com>
2026-04-13 15:42:32 +0200

    configuracion de i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n']
+
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+
+  i18n: {
+    defaultLocale: 'es',
+    langDir: 'locales',
+    locales: [
+      { code: 'es', name: 'Español', file: 'es.json' },
+      { code: 'en', name: 'English', file: 'en.json' }
+    ]
+  }
 })
